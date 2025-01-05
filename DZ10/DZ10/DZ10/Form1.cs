c856d182d34f6f3e4aa7f950226f799ec69acff5
itsPavko <jovan.jpk@gmail.com>
2025-01-05 22:54:26 +0100

    DZ10

---
+++ DZ10/DZ10/DZ10/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DZ10
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+
+			Random rnd = new Random();
+
+			for (int j = 0; j < 100; j++)
+			{
+				int n = rnd.Next(1, 100);
+				this.listBox1.Items.Add(n);
+			}
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			Random rnd = new Random();
+
+			int n = rnd.Next(1, 100);
+			this.listBox1.Items.Add(n);
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			int sel = this.listBox1.SelectedIndex;
+			this.listBox1.Items.RemoveAt(sel);
+
+		}
+	}
+}
