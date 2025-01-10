ac392f95fd2b3dfb164b2890fab71674423af27c
itsPavko <jovan.jpk@gmail.com>
2025-01-10 18:53:43 +0100

    DZ 13, 14, 15

---
+++ DZ15/DZ15/DZ15/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DZ15
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
+			float iznos;
+			float kamata;
+			try
+			{
+				iznos = float.Parse(textBox1.Text);
+				
+			}
+			catch (FormatException)
+			{
+				MessageBox.Show("Iznos nije broj", "Greska pri unosu iznosa",
+					MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			try
+			{
+				kamata = float.Parse(textBox2.Text);
+
+			}
+			catch (FormatException)
+			{
+				MessageBox.Show("Kamata nije broj", "Greska pri unosu kamate",
+					MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			float novIznos = iznos * ((100 + kamata)/100);
+			MessageBox.Show(novIznos.ToString(), "Resenje",
+				MessageBoxButtons.OK, MessageBoxIcon.Information);
+		}
+	}
+}
